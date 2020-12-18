0ccb04e4ef85205cd1d764d9e28f8b3d036190b2
shazy89 <erdo.shazy123@gmail.com>
2020-12-18 10:46:10 -0500

    Components Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>COOOL</Text>;
+  return <Text style={styles.text}>OK</Text>;
 };
 
 const styles = StyleSheet.create({
