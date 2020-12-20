7c89237ce687d35a42e9e67b7a98a136e623b704
shazy89 <erdo.shazy123@gmail.com>
2020-12-20 11:26:28 -0500

    exercise

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -6,12 +6,10 @@ const BoxScreen = () => {
 
 
    return (
-       <View style={styles.viewStyle}> 
-            <Text style={styles.text1Style}> Child #1</Text>
-            <Text style={styles.text2Style}> Child #2</Text>
-            <Text style={styles.text3Style}> Child #3</Text>
-            <Text style={styles.text3Style}> Child #1</Text>
-            <Text style={styles.text3Style}> Child #2</Text>
+       <View style={styles.parentStyle}> 
+            <View style={styles.view4Style} /> 
+            <View style={styles.view5Style} /> 
+            <View style={styles.view6Style} /> 
 
        </View>
    );
@@ -20,35 +18,70 @@ const BoxScreen = () => {
       
 
 const styles = StyleSheet.create({
-  viewStyle: {
+  parentStyle: {
 
       borderWidth: 3,
       borderColor: 'black',
-  //  alignItems: 'flex-end' 'center' 
+  //  alignItems: 'flex-end' 'center'  
       flexDirection: 'row', // remember always we have to place on parent element to set up the view
-      // flex direction will change the view horizontaly and verticaly
+ //   flex direction will change the view horizontaly and verticaly
       height: 200,
-      alignItems: 'center', // remember always we have to place on parent element to set up the view
+      alignItems: 'flex-start', // remember always we have to place on parent element to set up the view
       justifyContent: 'space-between' // remember always we have to place on parent element to set up the view
   },
 
   text1Style: {
       borderWidth: 3,
       borderColor: 'red',
-      flex: 1,
+      left: 10
+    //alignSelf: 'center'
+    //  flex: 4,
     },
   text2Style: {
       borderWidth: 3,
       borderColor: 'red',
-      flex: 1,
+     // left: 10,
+     // top: 0,
+     // left: 0,
+     // right: 0,
+     // position: 'absolute' //will ignore to its siblings
+       // this is the shortcut
+     ...StyleSheet.absoluteFillObject //same as the values commented aut left right top  bottom and position 
+    //alignSelf: 'flex-end'
+    //  flex: 4,
     },
   text3Style: {
+
       borderWidth: 3,
       borderColor: 'red',
-      flex: 1,
+      alignSelf: 'flex-start'
+    //  flex: 2,
     },
+    view4Style: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'blue',  
+    borderWidth: 3,
+    borderColor: 'red',
+ 
+    },
+    view5Style: {
+    borderWidth: 3,
+    height: 50,
+    width: 50,
+    borderColor: 'red',
+    backgroundColor: 'green', 
+    top: 50
+    },
+    view6Style: {
+    borderWidth: 3,
+    height: 50,
+    width: 50,
+    borderColor: 'red',
+    backgroundColor: 'black', 
+    }
   
-  })
+  });
       
 
 export default BoxScreen
